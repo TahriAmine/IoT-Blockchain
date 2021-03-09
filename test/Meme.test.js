558df675a3b8f15ce1c0559d89d6c843bdb6447c
tahri.amine <67011596+TahriAmine@users.noreply.github.com>
2021-03-09 14:43:50 +0100

    Add files via upload

--- test/Meme.test.js
+++ test/Meme.test.js
@@ -1,1 +1,19 @@
+const { Item } = require('react-bootstrap/lib/Breadcrumb')
 
+const Meme = artifacts.require("./Meme");
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Meme', (accounts) => {
+    let meme
+
+    describe('deployment', async()=>{
+        it('deploys successfully',async()=>{
+            meme = await Meme.deployed()
+            const address = meme.address
+            console.log(address)
+        })
+    })
+})
